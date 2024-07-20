eeff28d238279bb4112f0c4d3bea57fcd9f44bcf
Girish <girish@post.com>
2024-07-19 17:22:31 -0700

    4.8: Blog List Tests, step 1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const { after, beforeEach, test } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 
 const app = require('../app')
@@ -31,6 +32,75 @@ test('the first blog title is React Patterns', async () => {
   const titles = response.body.map(e => e.title)
 
   assert(titles.includes('React patterns'))
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'javascript',
+    url: 'www.example.com/async/await',
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, testBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+
+
+  assert(titles.includes('async/await simplifies making async calls'))
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'javascript',
+    url: 'www.example.com/blog/without/title',
+    likes: 20
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  assert.strictEqual(blogsAtEnd.length, testBlogs.length)
+})
+
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultBlog.body, blogToView)
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const titles = blogsAtEnd.map(t => t.title)
+  assert(!titles.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, testBlogs.length - 1)
 })
 
 beforeEach(async () => {
